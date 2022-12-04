d5aa285fcbfad4709e796ea150801d7ba96d6962
Ish Kwatra <77015916+ishkwatra@users.noreply.github.com>
2022-12-04 16:25:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let i = 20;
+if (i === 20) alert("Hello! Welcome to my first ever webpage!!!");
+alert("Check out the Console window for a surprise!")
+let j = 2022 - 2002;
+console.log("I am Ish, and my age is:", j);
